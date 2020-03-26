bac942bd4b7ad6b057358cb7c849915207359a76
peter14067 <peter14067@gmail.com>
2020-03-26 12:55:56 +0800

    Updates

---
+++ precache-manifest.b08af8f022edf830641726e80fa9b74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b084f0a2bb9b25f969953c7e2722620",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "5944f339fa9c66854382",
+    "url": "/chat/static/js/2.2ac982cb.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/chat/static/js/2.2ac982cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d796e49f30da1540715",
+    "url": "/chat/static/js/main.a6632bdc.chunk.js"
+  },
+  {
+    "revision": "f81e32e008a30fec5fcf",
+    "url": "/chat/static/js/runtime-main.7299832f.js"
+  },
+  {
+    "revision": "96a1a8305b1e2d0f1a63d30187aeef29",
+    "url": "/chat/static/media/mazep.96a1a830.jpg"
+  }
+]);
